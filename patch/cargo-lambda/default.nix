aff93900df6f5b6055b0e7e48a389e57cdca4c70
Paul Liljenberg <liljenberg.paul@gmail.com>
2023-09-02 11:42:34 +0200

    try fetchGit

--- patch/cargo-lambda/default.nix
+++ patch/cargo-lambda/default.nix
@@ -2,7 +2,7 @@
 , cacert
 , curl
 , rustPlatform
-, fetchgit
+, fetchGit
 , makeWrapper
 , pkg-config
 , openssl
@@ -16,7 +16,7 @@ rustPlatform.buildRustPackage rec {
   pname = "cargo-lambda";
   version = "6b11f264f324581a0112dd8cdbd0e125a55369ef";
 
-  src = fetchgit {
+  src = fetchGit {
     url = "https://github.com/coactdev/cargo-lambda.git";
     rev = "${version}";
     ref = "dev";
